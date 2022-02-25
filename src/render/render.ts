d28f850c6873e09a9528f3f742b4f0b2316231d2
Michel Drescher <michel.drescher@gmail.com>
2022-02-25 18:30:49 +0100

    Refactoring; break ring path into individual elements for styling

--- src/render/render.ts
+++ src/render/render.ts
@@ -1,7 +1,7 @@
 import { SVG } from '@svgdotjs/svg.js'
 
 import { Options } from '../types'
-import { calcAngles } from '../util/math'
+import { calcAngles, polar2cartesian } from '../util/math'
 
 //
 // constructs the complete radar based on the given segments, rings, and options
@@ -47,12 +47,20 @@ const addSegment = function (
     opts: Options
 ): void {
     // 1) create the segment's group
-    const seg = root.group().attr({ label: name, class: `segment segment-${idx}` })
+    const seg = root.group().attr({
+        label: name,
+        class: `segment segment-${idx}`,
+        'data-angle-start': `${angles[idx]}`,
+        'data-angle-end': `${angles[idx + 1]}`,
+    })
     // 2) add the rings
-    let last: any
     for (let i = 0; i < radii.length - 1; i++) {
-        const ring = seg.group().attr({ class: `ring ring-${i}` })
-        last = addRing(ring, angles[idx], angles[idx + 1], radii[i], radii[i + 1])
+        const ring = seg.group().attr({
+            class: `ring ring-${i}`,
+            'data-radius-inner': `${radii[i]}`,
+            'data-radius-outer': `${radii[i + 1]}`,
+        })
+        addRing(ring, angles[idx], angles[idx + 1], radii[i], radii[i + 1])
     }
     // 3) add the segment lines
     addSegmentLines(seg, angles[idx], angles[idx + 1], radii[radii.length - 1], opts.subSegments)
@@ -100,23 +108,60 @@ const addSegmentLines = (
 //
 // Add a ring to a segment
 //
-const addRing = function (root: any, startA: number, endA: number, startR: number, endR: number) {
-    console.log(startA, endA, startR, endR)
-    // find the start and en coords for the lower arc
-    let x = startR * Math.cos(startA - Math.PI / 2)
-    let y = startR * Math.sin(startA - Math.PI / 2)
-    // compile the first arc commands
-    let path = `M${x} ${y} ${arcPath(endA, startR, 1)} `
-    // add the "right" line up to the upper arch
-    x = endR * Math.cos(endA - Math.PI / 2)
-    y = endR * Math.sin(endA - Math.PI / 2)
-    // console.log(endX, endY)
-    path += `L${x} ${y} `
-    // add the "upper" arc and close the path
-    path += `${arcPath(startA, endR, 0)} Z`
-    // add the path to the SVG
-    const ring = root.path(path)
-    return ring
+const addRing = function (
+    root: any,
+    startA: number,
+    endA: number,
+    startR: number,
+    endR: number
+): void {
+    // ORIGINAL
+    // <path d="M3.631077759471902e-14 -593 A593 593 0 0 1 513.5530644441722 296.4999999999999 L628.7344431475025 362.9999999999999 A726 726 0 0 0 4.445467880904892e-14 -726 Z"></path>
+    //
+    // TARGET
+    // <path d="M3.631077759471902e-14 -593 A593 593 0 0 1 513.5530644441722 296.4999999999999"></path>
+    // <line x1="513.5530644441722" y1="296.4999999999999" x2="628.7344431475025" y2="362.9999999999999"></line>
+    // <path d="M628.7344431475025 362.9999999999999 A726 726 0 0 0 4.445467880904892e-14 -726"></path>
+    // <line x1="4.445467880904892e-14" y1="-726" x2="3.631077759471902e-14" y2="-593"></line>
+
+    // find the four coordinates for the ring
+    const _1 = polar2cartesian(startR, startA)
+    const _2 = polar2cartesian(startR, endA)
+    const _3 = polar2cartesian(endR, endA)
+    const _4 = polar2cartesian(endR, startA)
+
+    // coordinates for the ring stroke
+    const w = endR - startR
+    const r = startR + w / 2
+    const _5 = polar2cartesian(r, startA)
+    const _6 = polar2cartesian(r, endA)
+
+    // add the arc that "fills" the actual arc
+    root.path(`M ${_5.x} ${_5.y} A ${r} ${r} 0 0 1 ${_6.x} ${_6.y}`).attr({
+        'stroke-width': `${w}`,
+        fill: 'none',
+        class: 'arc-stroke',
+    })
+
+    // add inner arc
+    root.path(`M ${_1.x} ${_1.y} A ${startR} ${startR} 0 0 1 ${_2.x} ${_2.y}`).attr({
+        class: 'arc-inner',
+    })
+
+    // outer arc
+    root.path(`M ${_4.x} ${_4.y} A ${endR} ${endR} 0 0 1 ${_3.x} ${_3.y}`).attr({
+        class: 'arc-outer',
+    })
+
+    // left line
+    root.line(_1.x, _1.y, _4.x, _4.y).attr({
+        class: 'left-line',
+    })
+
+    // right line
+    root.line(_2.x, _2.y, _3.x, _3.y).attr({
+        class: 'right-line',
+    })
 }
 
 //
@@ -140,14 +185,13 @@ const addSegmentName = (
     opts: Options
 ): void => {
     // need to recalculate the last arc path...
-    let x = radius * Math.cos(startA - Math.PI / 2)
-    let y = radius * Math.sin(startA - Math.PI / 2)
-    const path = `M${x} ${y} ${arcPath(endA, radius, 1)} `
-
+    const _1 = polar2cartesian(radius, startA)
+    const _2 = polar2cartesian(radius, endA)
     // now add the text path to the segment group
     const text = root.text()
     text.font({ size: opts.labelSize, anchor: 'middle' })
-    const tspan = text.tspan(name).dy(-1 * opts.labelOffset)
-    const textPath = text.path(path)
-    textPath.attr({ startOffset: '50%' })
+    text.tspan(name).dy(-1 * opts.labelOffset)
+    text.path(`M${_1.x} ${_1.y} A${radius} ${radius} 0 0 1 ${_2.x} ${_2.y}`).attr({
+        startOffset: '50%',
+    })
 }
--- src/util/math.ts
+++ src/util/math.ts
@@ -1,24 +1,44 @@
 //
+// a simple cartesian coordinate pair
+//
+export type Coord = {
+    x: number // each blip has a numeric ID...
+    y: number // .. and a name.
+}
+
+//
 // convert an angle and egrees into radians
 //
-export const toRadian = (angleDeg: number): number => (Math.PI * angleDeg) / 180;
+export const toRadian = (angleDeg: number): number => (Math.PI * angleDeg) / 180
 
 //
 // convert an angle in rad to degree
 //
-export const toDegree = (angleRad: number): number => (angleRad * 180) / Math.PI;
+export const toDegree = (angleRad: number): number => (angleRad * 180) / Math.PI
 
 //
 // calculate the RADIAN angles for the radar segments using the given values
 //
 export const calcAngles = (numSegments: number): number[] => {
-  const angle = toRadian(360 / numSegments);
-  if (numSegments < 1) return [];
+    const angle = toRadian(360 / numSegments)
+    if (numSegments < 1) return []
 
-  const result = [0];
-  for (let i = 0; i < numSegments; i++) {
-    result.push(result[result.length - 1] + angle);
-  }
+    const result = [0]
+    for (let i = 0; i < numSegments; i++) {
+        result.push(result[result.length - 1] + angle)
+    }
 
-  return result;
-};
+    return result
+}
+
+//
+// translates polar coordinates given in radius and angle (radians) into
+// cartesian coordinates, rotated by -90 degrees (i.e. reference line is the y axis, not the x axis)
+// Returns an oject { x: number, y: number }
+//
+export function polar2cartesian(radius: number, angle: number): Coord {
+    return {
+        x: radius * Math.cos(angle - Math.PI / 2),
+        y: radius * Math.sin(angle - Math.PI / 2),
+    }
+}
